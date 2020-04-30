7ec3e1855e680cd799c2d084b9048698a652d17d
Yoel Monzon <yoelfme@hotmail.com>
2020-04-29 19:58:31 -0500

    [add] styled components for nav, header and page

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,14 +1,24 @@
 import Link from 'next/link'
+import NavStyles from './styles/NavStyles'
 
 const Nav = () => (
-    <div>
-        <Link href="/sell">
-            Sell
-        </Link>
-        <Link href="/">
-            Home
-        </Link>
-    </div>
+  <NavStyles>
+    <Link href="items">
+      <a>Items</a>
+    </Link>
+    <Link href="sell">
+      <a>Sell</a>
+    </Link>
+    <Link href="signup">
+      <a>Signup</a>
+    </Link>
+    <Link href="orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="me">
+      <a>Me</a>
+    </Link>
+  </NavStyles>
 )
 
 export default Nav
